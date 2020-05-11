bc953c8648f18f1fb0878e66b4d3b41c5bf2c767
classlfz <classlfz@qq.com>
2020-05-11 17:01:46 +0800

    :sparkles:add services api & use protobufjs replace protoc

--- develop.md
+++ develop.md
@@ -1,23 +1,30 @@
 ## 目录
 
 ```
++-- jsons   // pbjsons
 +-- lib     // 主目录
-    |-- helpers // 一些辅助函数
-    |-- modules // 对 编译好的文件 的一层封装
-    |-- proto   // 已编译好的 `**pb.js` 文件
-    |-- example-modules.js  // 使用 modules 的示例 
-    |-- example-proto.js    // 使用 proto 的示例 
+    |-- helper // 一些辅助函数
+    |-- DevicedService // deviced
+    |-- EvaluatordService // evaluatord
+    |-- Identityd2Service // identityd
+    |-- MtpdService // mtpd
+    |-- TagdService // tagd
+    |-- Service // Service类
+    |-- StreamCall // StreamCall类
 ```
 
 ## 开发
 
 - clone proto
-  ```
-  git clone https://github.com/bingliu221/metathings-proto.git ./sources/proto
-  ```
-- 修改 sources/proto 中目录层级至需要的层级，并修改对应的引用路径
-  > 暂时对于层级的处理为放在同级，即对应 metathings 的结构
+
+  - 最好能安装好 go 开发 `metathings` 的环境，克隆 `metathings` 库下来，然后安装好其protos所需要的依赖防止与`$GOPATH/src/github.com`下边；
+
+  - 如果选择不去安装go的开发环境，可选择直接克隆metathings库到某处，然后在每一个proto的编译环境下添加对应的依赖文件，从而进行一一编译，但不推荐此方法；
+
 - 执行编辑
+
+  这里的编译脚本采用的是上边所说的第一种方式；
+
   ```
   npm run generate
   ```
--- generate.sh
+++ generate.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+rm -rf jsons/
+mkdir jsons
 
-# rm -rf ./lib/proto/*
-# cp -r ./lib/proto ./lib/proto_backup
-
-find ./sources/proto/ -name *.proto | xargs ./node_modules/.bin/grpc_tools_node_protoc -I=./sources/proto --js_out=import_style=commonjs,binary:./lib/proto
-find ./sources/proto/ -name service.proto | xargs ./node_modules/.bin/grpc_tools_node_protoc -I=./sources/proto --js_out=import_style=commonjs,binary:./lib/proto --grpc_out=./lib/proto
+./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/deviced/service.proto > ./jsons/deviced_service_pb.json
+./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/evaluatord/service.proto > ./jsons/evaluatord_server_pb.json
+./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/identityd2/service.proto > ./jsons/identityd2_service_pb.json
+./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/tagd/service.proto > ./jsons/tagd_service_pb.json
+./node_modules/.bin/pbjs -t json -p ./proto/mtpd/service.proto > ./jsons/mtpd_service_pb.json
