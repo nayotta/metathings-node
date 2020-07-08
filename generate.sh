67595def25d182158870f92b6218f81cd5cfa663
classlfz <classlfz@qq.com>
2020-07-08 16:43:27 +0800

    :arrow_up: update evaluatord pb json

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@ rm -rf jsons/
 mkdir jsons
 
 ./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/deviced/service.proto > ./jsons/deviced_service_pb.json
-./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/evaluatord/service.proto > ./jsons/evaluatord_service.json
+./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/evaluatord/service.proto > ./jsons/evaluatord_service_pb.json
 ./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/identityd2/service.proto > ./jsons/identityd2_service_pb.json
 ./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/tagd/service.proto > ./jsons/tagd_service_pb.json
-./node_modules/.bin/pbjs -t json -p ./proto/mtpd/service.proto > ./jsons/mtpd_service_pb.json
+./node_modules/.bin/pbjs -t json proto/mtpd/service.proto > ./jsons/mtpd_service_pb.json
