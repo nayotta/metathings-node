2bfdd23cc30a453f66206f32b9a4941fa77370d5
classlfz <classlfz@qq.com>
2021-05-19 17:53:47 +0800

    :art: update pb jsons

--- generate.sh
+++ generate.sh
@@ -1,8 +1,8 @@
 rm -rf jsons/
 mkdir jsons
 
-./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/deviced/service.proto > ./jsons/deviced_service_pb.json
-./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/evaluatord/service.proto > ./jsons/evaluatord_service_pb.json
-./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/identityd2/service.proto > ./jsons/identityd2_service_pb.json
-./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/pkg/proto/tagd/service.proto > ./jsons/tagd_service_pb.json
+./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/proto/deviced/service.proto > ./jsons/deviced_service_pb.json
+./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/proto/evaluatord/service.proto > ./jsons/evaluatord_service_pb.json
+./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/proto/identityd2/service.proto > ./jsons/identityd2_service_pb.json
+./node_modules/.bin/pbjs -t json -p $GOPATH/src metathings/proto/tagd/service.proto > ./jsons/tagd_service_pb.json
 ./node_modules/.bin/pbjs -t json proto/mtpd/service.proto > ./jsons/mtpd_service_pb.json
